d2904363c206213493934f5d6f9f5f3648b093be
casualgamer96 <valenciabrandon331@gmail.com>
2024-10-01 00:35:54 -0400

    Update Group_Assignment.cpp

--- Module_005/Group Assignment/Group_Assignment.cpp
+++ Module_005/Group Assignment/Group_Assignment.cpp
@@ -1,8 +1,20 @@
-/*
-*/
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
+  double interstRate; //interst rate given
+  double startingBalance; //
+  int months; //time since account was established
+
+  cout<<"Please enter the annual interest rate: "<<endl;
+  cin>>interstRate;
+  
+  cout<<"Please enter the starting balance: "<<endl;
+  cin>>startingBalance;
+  
+  cout<<"Please enter the number of months since the account was established: "<<endl;
+  cin>>months;
+
 }
